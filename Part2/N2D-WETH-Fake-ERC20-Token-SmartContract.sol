874226743862de67bb94b0913bed1a9f5666c602
Net2Dev <info@a3b.io>
2023-06-14 12:12:17 -0700

    Add files via upload

---
+++ Part2/N2D-WETH-Fake-ERC20-Token-SmartContract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT LICENSE
+
+/*
+Follow/Subscribe Youtube, Github, IM, Tiktok
+for more amazing content!!
+@Net2Dev
+███╗░░██╗███████╗████████╗██████╗░██████╗░███████╗██╗░░░██╗
+████╗░██║██╔════╝╚══██╔══╝╚════██╗██╔══██╗██╔════╝██║░░░██║
+██╔██╗██║█████╗░░░░░██║░░░░░███╔═╝██║░░██║█████╗░░╚██╗░██╔╝
+██║╚████║██╔══╝░░░░░██║░░░██╔══╝░░██║░░██║██╔══╝░░░╚████╔╝░
+██║░╚███║███████╗░░░██║░░░███████╗██████╔╝███████╗░░╚██╔╝░░
+╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚══════╝╚═════╝░╚══════╝░░░╚═╝░░░
+THIS CONTRACT IS AVAILABLE FOR EDUCATIONAL 
+PURPOSES ONLY. YOU ARE SOLELY REPONSIBLE 
+FOR ITS USE. I AM NOT RESPONSIBLE FOR ANY
+OTHER USE. THIS IS TRAINING/EDUCATIONAL
+MATERIAL. ONLY USE IT IF YOU AGREE TO THE
+TERMS SPECIFIED ABOVE.
+*/
+
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+pragma solidity ^0.8.17.0;
+
+contract WETH is ERC20, ERC20Burnable, Ownable {
+
+  using SafeERC20 for ERC20;
+
+  constructor() ERC20("Wrapped ETH", "WETH") {}
+
+  function mint(uint256 amount) external onlyOwner {
+    _mint(msg.sender, amount);
+  }
+
+}
