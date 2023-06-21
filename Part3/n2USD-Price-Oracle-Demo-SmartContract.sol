546a80fa3160a9a612814c9f9025872aa249fdeb
Net2Dev <info@a3b.io>
2023-06-21 11:31:02 -0700

    Update n2USD-Price-Oracle-Demo-SmartContract.sol

--- Part3/n2USD-Price-Oracle-Demo-SmartContract.sol
+++ Part3/n2USD-Price-Oracle-Demo-SmartContract.sol
@@ -1,5 +1,23 @@
 // SPDX-License-Identifier: MIT LICENSE
 
+/*
+Follow/Subscribe Youtube, Github, IM, Tiktok
+for more amazing content!!
+@Net2Dev
+███╗░░██╗███████╗████████╗██████╗░██████╗░███████╗██╗░░░██╗
+████╗░██║██╔════╝╚══██╔══╝╚════██╗██╔══██╗██╔════╝██║░░░██║
+██╔██╗██║█████╗░░░░░██║░░░░░███╔═╝██║░░██║█████╗░░╚██╗░██╔╝
+██║╚████║██╔══╝░░░░░██║░░░██╔══╝░░██║░░██║██╔══╝░░░╚████╔╝░
+██║░╚███║███████╗░░░██║░░░███████╗██████╔╝███████╗░░╚██╔╝░░
+╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚══════╝╚═════╝░╚══════╝░░░╚═╝░░░
+THIS CONTRACT IS AVAILABLE FOR EDUCATIONAL 
+PURPOSES ONLY. YOU ARE SOLELY REPONSIBLE 
+FOR ITS USE. I AM NOT RESPONSIBLE FOR ANY
+OTHER USE. THIS IS TRAINING/EDUCATIONAL
+MATERIAL. ONLY USE IT IF YOU AGREE TO THE
+TERMS SPECIFIED ABOVE.
+*/
+
 pragma solidity ^0.8.18.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
